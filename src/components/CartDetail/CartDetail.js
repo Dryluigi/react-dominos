fb2ad776092c964d7cc9021e5e35471470e958d0
dryluigi <marioprasetyamulya85@gmail.com>
2021-08-05 20:58:42 +0700

    Optimizing code in CartDetail

--- src/components/CartDetail/CartDetail.js
+++ src/components/CartDetail/CartDetail.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './CartDetail.css';
 
@@ -7,11 +7,9 @@ import { MainAside, Main, Aside } from '../UI/Layout/MainAside';
 import CartDetailHeader from './CartDetailHeader/CartDetailHeader';
 import CartFoodItem from './CartFoodItem/CartFoodItem';
 import TotalInformation from './TotallInformation/TotalInformation';
-import { checkoutActions } from '../../store/checkout/checkout-slice';
 import cartFoodsSort from '../../lib/cart-foods-sort';
 
 const CartDetail = () => {
-  const dispatch = useDispatch();
   const cartFoods = useSelector(state => state.cart.foods);
 
   const sortedCartFoods = cartFoodsSort(cartFoods);
